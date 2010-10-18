f17c89b1caa58300f23ad50d7e65c97b6da86eec
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@b139f23c-5e1e-54d6-eab5-85b03e268133>
2010-10-18 12:36:06 +0000

    Relatorio Iniciado. Iniciação do código final para ser apresentado. TODO: Documentação do Código. Screeners da aplicação a ser executada em diversos ambientes. Desenvolvimento do código para correr em multi-process.

---
+++ trunk/SO/Trabalhos/Trabalho 1/src/trabalhos/Include/BeginThreadex.h
@@ -0,0 +1,28 @@
+#ifndef _BEGINTHREADEX_H
+#define _BEGINTHREADEX_H
+
+#include <process.h>
+
+///////////////////////////// chBEGINTHREADEX Macro ///////////////////////////
+
+
+// This macro function calls the C runtime's _beginthreadex function. 
+// The C runtime library doesn't want to have any reliance on Windows' data 
+// types such as HANDLE. This means that a Windows programmer needs to cast
+// values when using _beginthreadex. Since this is terribly inconvenient, 
+// I created this macro to perform the casting.
+typedef unsigned (__stdcall *PTHREAD_START) (void *);
+
+#define chBEGINTHREADEX(psa, cbStack, pfnStartAddr, \
+   pvParam, fdwCreate, pdwThreadId)                 \
+      ((HANDLE)_beginthreadex(                      \
+         (void *)        (psa),                     \
+         (unsigned)      (cbStack),                 \
+         (PTHREAD_START) (pfnStartAddr),            \
+         (void *)        (pvParam),                 \
+         (unsigned)      (fdwCreate),               \
+         (unsigned *)    (pdwThreadId)))
+
+#define chENDTHREADEX(x) ( _endthreadex((unsigned) x))
+
+#endif
