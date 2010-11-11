635770b2ff5f0115c204f9189250e8814f70dc2d
the.whinner <the.whinner@b139f23c-5e1e-54d6-eab5-85b03e268133>
2010-11-11 19:41:16 +0000

    Avancei mais um bocado. Aqui estao os updates.

--- trunk/SO/Trabalhos/Trabalho 2/tmp_src/31529/versao1/Trab2/IGestorDePistas.h
+++ trunk/SO/Trabalhos/Trabalho 2/tmp_src/31529/versao1/Trab2/IGestorDePistas.h
@@ -3,9 +3,11 @@
 
 class IGestorDePistas {
 public:
-	virtual int esperarPistaParaAterrar () = 0;
-	virtual int esperarPistaParaDescolar ()= 0;
-	virtual void libertarPista (int idPista)= 0;
+
+	enum PlaneDirection { LAND,LIFTOFF,LAND_AND_LIFTOFF };
+
+	virtual int UseLaneTo(PlaneDirection direction) = 0;
+	virtual void SetLanePriorityTo (PlaneDirection direction) = 0;
 	virtual void fecharPista (int idPista) = 0;
 	virtual void abrirPista (int idPista) = 0;
 };
