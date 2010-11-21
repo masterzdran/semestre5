7422d9efff3c7802a363a463be44d342faf46ded
the.whinner <the.whinner@b139f23c-5e1e-54d6-eab5-85b03e268133>
2010-11-21 04:17:14 +0000

    Update. Tentativa de criar um teste para o gestor um pouco falhada. :z

--- SO/Trabalhos/Trabalho 2/tmp_src/31529/versao1/Trab2/IGestorDePistas.h
+++ SO/Trabalhos/Trabalho 2/tmp_src/31529/versao1/Trab2/IGestorDePistas.h
@@ -6,12 +6,12 @@
 class IGestorDePistas {
 public:
 
-	enum PlaneDirection { LAND,LIFTOFF,LAND_AND_LIFTOFF };
+	static enum PlaneDirection { LAND,LIFTOFF,LAND_AND_LIFTOFF };
 
 	virtual Plane * LandPlane() = 0;
 	virtual Plane * LiftoffPlane() = 0;
 	virtual void ReleaseLaneUsedBy(Plane * p) = 0;
-	virtual void SetLanePriorityTo (PlaneDirection direction) = 0;
+	virtual bool SetLanePriorityTo (PlaneDirection direction, int idLane) = 0;
 	virtual void fecharPista (int idPista) = 0;
 	virtual void abrirPista (int idPista) = 0;
 };
