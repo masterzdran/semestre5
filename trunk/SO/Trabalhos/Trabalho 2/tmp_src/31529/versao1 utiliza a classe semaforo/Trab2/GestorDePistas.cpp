ae6f18b2441eb1f72a008c75df078b29b345b275
the.whinner <the.whinner@b139f23c-5e1e-54d6-eab5-85b03e268133>
2010-12-06 17:39:29 +0000

    bla

---
+++ trunk/SO/Trabalhos/Trabalho 2/tmp_src/31529/versao1 utiliza a classe semaforo/Trab2/GestorDePistas.cpp
@@ -0,0 +1,288 @@
+#include "stdafx.h"
+
+#ifndef GESTORDEPISTAS_CPP
+#define GESTORDEPISTAS_CPP
+
+class GestorDePistas : public IGestorDePistas
+{
+	char _nLanes;
+	//semaforos que vao bloquear as threads
+	Semaforo * _sWaitingListLanding;
+	Semaforo * _sWaitingListLiftoff;
+	//mutexs para aceder 'as listas
+	Semaforo * _mPlaneListToLand;
+	Semaforo * _mPlaneListToLift;
+	//listas de avioes para aterrar/descolar
+	Plane * _planeListToLand;
+	volatile LONG _countPlaneToLand;
+	Plane * _planeListToLift;
+	volatile LONG _countPlaneToLift;
+	Plane * _planesOnTheGround;
+	//nº de avioes criados ate' ao momento.
+	long _planeCount;
+	bool _bFuracao;
+	/***************************************/
+	/********Criar um objecto pista?!*******/
+	/***************************************/
+		//mutex para aceder 'as variaveis associadas a uma pista.
+		Semaforo * _mLanes;
+		//indica se a pista esta em uso
+		bool * _bLanes;
+		//direccao prioritaria da pista
+		Plane::PlaneDirection * _pdLanes;
+	/***************************************/
+
+	/********TESTAR********/
+	Plane* AddNewPlaneInEndOf(Plane * planeList, Plane::PlaneDirection pd)
+	{
+		Plane * p = new Plane(pd,InterlockedIncrement(&_planeCount));
+		AddPlaneIn(planeList,p);
+		return p;
+	}
+	Plane* AddPlaneIn(Plane * planeList,Plane * p)
+	{
+		planeList->prev->next = p;
+		p->prev = planeList->prev;
+		p->next = planeList;
+		planeList->prev = p;
+		return p;
+	}
+	/********TESTAR********/
+	/***PRINCIPALMENTE A REMOCAO DO ULTIMO ELEMENTO***/
+	void Remove(Plane * elemToRemove)
+	{
+		int id = elemToRemove->_idPlane;
+
+		if(elemToRemove->prev==elemToRemove)
+		{
+			//significa que é o sentinela, logo não é para remover
+			return;
+		}
+
+		elemToRemove->next->prev = elemToRemove->prev;
+		elemToRemove->prev->next = elemToRemove->next;
+	}
+
+	int findLaneTo(Plane::PlaneDirection direction)
+	{
+		for(int i = 0;i<_nLanes;++i)
+		{
+			if((_pdLanes[i] == direction || _bFuracao) && _bLanes[i])
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	//As threads são desbloqueadas através de um método que indicará 
+	//qual a pista a libertar e consoante a sua prioridade irá libertar
+	//uma thread da lista correspondente ou uma de outra, caso não haja.
+	Plane * UseLaneTo(Plane::PlaneDirection direction, Plane * planeList,
+		Semaforo * mPlaneList, Semaforo * sWaitingList)
+	{
+		Plane * p;
+		do{
+			_mLanes->Wait();
+			int nLane = findLaneTo(direction);
+			if(nLane == -1)
+			{
+				if(direction==Plane::LAND && _planeListToLift->next==_planeListToLift)
+				{
+					nLane = findLaneTo(Plane::LIFTOFF);
+				}
+				else if(direction==Plane::LIFTOFF && _planeListToLand->next==_planeListToLand)
+				{
+					nLane = findLaneTo(Plane::LAND);
+				}
+				
+				if(nLane==-1)
+				{
+					_mLanes->Signal();
+					sWaitingList->Wait();
+				}
+			}
+
+			if(nLane!=-1)
+			{
+				_bLanes[nLane]=false;
+				_mLanes->Signal();
+				//obter o aviao consoante a direccao
+				mPlaneList->Wait();
+				p = planeList->next;
+				Remove(p);
+				mPlaneList->Signal();
+				p->_finishedWork = true;
+				p->_idLane = nLane;
+				break;
+			}
+		}while(true);
+		
+		return p;
+	}
+
+	void SignalRespectiveWaitingList(Plane::PlaneDirection pd)
+	{
+		if(_bFuracao || pd==Plane::LAND && _planeListToLand->next != _planeListToLand 
+			|| pd==Plane::LIFTOFF && _planeListToLift->next == _planeListToLift)
+		{
+			_sWaitingListLanding->Signal();
+		}
+		else if(!_bFuracao && (pd==Plane::LIFTOFF && _planeListToLift->next != _planeListToLift 
+			|| pd==Plane::LAND && _planeListToLand->next == _planeListToLand))
+		{
+			_sWaitingListLiftoff->Signal();
+		}
+	}
+
+public:
+
+	GestorDePistas(int nLanes)
+	{
+		_nLanes = nLanes;
+		_sWaitingListLanding = new Semaforo(0);
+		_sWaitingListLiftoff = new Semaforo(0);
+		_mPlaneListToLand = new Semaforo(1,1);
+		_mPlaneListToLift = new Semaforo(1,1);
+		
+		_planeListToLift = new Plane();
+		_planeListToLift->next = _planeListToLift;
+		_planeListToLift->prev = _planeListToLift;
+		_countPlaneToLift = 0;
+
+		_planeListToLand = new Plane();
+		_planeListToLand->next = _planeListToLand;
+		_planeListToLand->prev = _planeListToLand;
+		_countPlaneToLand = 0;
+
+		_planesOnTheGround = new Plane();
+		_planesOnTheGround->next = _planesOnTheGround;
+		_planesOnTheGround->prev = _planesOnTheGround;
+
+		_planeCount=0;
+		_bFuracao=false;
+
+		_bLanes = new bool[nLanes];
+		for(int i = 0;i<nLanes;++i)
+			_bLanes[i]=true;
+		_mLanes = new Semaforo(1,1);
+		_pdLanes = new Plane::PlaneDirection[nLanes];		
+	}
+
+	virtual Plane * criarAviaoPara(Plane::PlaneDirection direction)
+	{
+		Semaforo * mPlaneList;
+		Plane * planeList;
+		if(direction==Plane::LAND)
+		{
+			mPlaneList = _mPlaneListToLand;
+			planeList = _planeListToLand;
+			InterlockedIncrement(&_countPlaneToLand);
+		}
+		else if(direction==Plane::LIFTOFF)
+		{
+			InterlockedIncrement(&_countPlaneToLift);
+			mPlaneList = _mPlaneListToLift;
+			planeList = _planeListToLift;
+		}
+		mPlaneList->Wait();
+		Plane * p = AddNewPlaneInEndOf(planeList,direction);
+		p->_finishedWork = false;
+		mPlaneList->Signal();
+		return p;
+	}
+	virtual Plane * esperarPistaParaAterrar()
+	{
+		Plane * p = UseLaneTo(Plane::LAND,_planeListToLand,_mPlaneListToLand,_sWaitingListLanding);
+		InterlockedDecrement(&_countPlaneToLand);
+		return p;
+	}
+
+	virtual Plane * esperarPistaParaDescolar()
+	{
+		Plane * p = UseLaneTo(Plane::LIFTOFF,_planeListToLift,_mPlaneListToLift,_sWaitingListLiftoff);
+		InterlockedDecrement(&_countPlaneToLift);
+		return p;
+	}
+
+	virtual INT getCountPlanesToLand()
+	{
+		return (INT)_countPlaneToLand;
+	}
+	virtual INT getCountPlanesToLift()
+	{
+		return (INT)_countPlaneToLift;
+	}
+	virtual void libertarPista(Plane * p)
+	{
+		if(!p->_finishedWork)
+			return;
+		_mLanes->Wait();
+		_bLanes[p->_idLane]=true;
+		Plane::PlaneDirection pd = _pdLanes[p->_idLane];
+		_mLanes->Signal();
+
+		SignalRespectiveWaitingList(pd);
+		//DELETE TÁ A ATRUFIAR ISTO.
+		//delete p;
+	}
+
+	virtual Plane::PlaneDirection getLaneDirectionUsedBy(Plane* p)
+	{
+		Plane::PlaneDirection direction;
+		_mLanes->Wait();
+		direction = _pdLanes[p->_idLane];
+		_mLanes->Signal();
+
+		return direction;
+	}
+	virtual BOOL SetLanePriorityTo (Plane::PlaneDirection direction, int idLane)
+	{
+		_mLanes->Wait();
+		//if (!_bLanes[idLane])
+		//{
+		//	_mLanes->Signal();
+		//	return false;
+		//}
+		_pdLanes[idLane] = direction;
+		_mLanes->Signal();
+		return true;
+	}
+
+	virtual void fecharPista (int idPista)
+	{
+		_mLanes->Wait();
+		if(_pdLanes[idPista]==Plane::LAND)
+			_pdLanes[idPista]=Plane::LAND_CLOSED;
+		else if (_pdLanes[idPista]==Plane::LIFTOFF)
+			_pdLanes[idPista]=Plane::LIFT_CLOSED;
+		_mLanes->Signal();
+	}
+
+	virtual void abrirPista (int idPista)
+	{
+		_mLanes->Wait();
+		if(_pdLanes[idPista]==Plane::LAND_CLOSED)
+			_pdLanes[idPista]=Plane::LAND;
+		else if (_pdLanes[idPista]==Plane::LIFT_CLOSED)
+			_pdLanes[idPista]=Plane::LIFTOFF;
+
+		SignalRespectiveWaitingList(_pdLanes[idPista]);
+		_mLanes->Signal();
+	}
+	virtual void alertaFuracao(bool bFuracao)
+	{
+		//se alguma das pistas se encontrar fechada os aviões irão aterrar nela de qualquer forma.
+		_bFuracao=bFuracao;
+		_mLanes->Wait();
+		for(int i = 0;i<N_LANES;++i)
+		{
+			if(_bLanes[i])
+			{
+				SignalRespectiveWaitingList(_pdLanes[i]);
+			}
+		}
+		_mLanes->Signal();
+	}
+};
+#endif GESTORDEPISTAS_CPP
---
+++ trunk/SO/Trabalhos/Trabalho 2/tmp_src/31529/versao2 utiliza a WINAPI/Trab2.cpp
@@ -0,0 +1,286 @@
+// Trab2.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "Trab2.h"
+
+
+#define MAX_LOADSTRING 100
+#define MAX_BUFFER 21
+
+// Global Variables:
+HINSTANCE hInst;								// current instance
+TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+
+GestorDePistas * gestor;
+//Objecto de sincronização utilizado para assegurar de que há somente uma thread de cada vez
+//a remover itens da ListBox.
+CRITICAL_SECTION csDelFromLB;
+//buffer utilizado para efectuar as leituras dos objectos existentes na parte gráfica
+TCHAR buffer[MAX_BUFFER];
+//indica se o alerta de furacao esta ligado ou não
+bool _bFuracao;
+
+typedef INT (*CountPlanesFunc)(void);
+typedef Plane * (*LandOrLiftPlanesFunc)(void);
+
+// Forward declarations of functions included in this code module:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	// criar Dialog do 2º trabalho prático
+	//DialogBox(hInst, MAKEINTRESOURCE(IDD_TRAB2_DIALOG), NULL, About);
+	DialogBox(hInst, MAKEINTRESOURCE(IDD_RUNWAY), NULL, About);
+	return (int) 0;
+}
+
+
+INT Landing_Animate_Id[] = {
+    IDC_AT0,  IDC_AT1,  IDC_AT2,  IDC_AT3,  IDC_AT4,  IDC_AT5,  IDC_AT6,  IDC_AT7,  IDC_AT8,
+    IDC_AT9,  IDC_AT10, IDC_AT11, IDC_AT12, IDC_AT13, IDC_AT14, IDC_AT15, IDC_AT16, IDC_AT17,
+    IDC_AT18, IDC_AT19, IDC_AT20, IDC_AT21, IDC_AT22, IDC_AT23, IDC_AT24, IDC_AT25
+}
+, Takeoff_Animate_Id[] = {
+    IDC_DS0,  IDC_DS1,  IDC_DS2,  IDC_DS3,  IDC_DS4,  IDC_DS5,  IDC_DS6,  IDC_DS7,  IDC_DS8,
+    IDC_DS9,  IDC_DS10, IDC_DS11, IDC_DS12, IDC_DS13, IDC_DS14, IDC_DS15, IDC_DS16, IDC_DS17,
+    IDC_DS18, IDC_DS19, IDC_DS20, IDC_DS21, IDC_DS22, IDC_DS23, IDC_DS24, IDC_DS25
+};
+
+
+void loadIntoBufferPlanesCount(Plane::PlaneDirection direction)
+{
+	int n;
+	if(Plane::LAND==direction)
+	{
+		n = gestor->getCountPlanesToLand();
+	}
+	else if(Plane::LIFTOFF==direction)
+	{
+		n = gestor->getCountPlanesToLift();
+	}
+	_itot(n,buffer,10);
+}
+
+Plane* executeLandOrListFunction(Plane::PlaneDirection direction)
+{
+	if(Plane::LAND==direction)
+	{
+		return gestor->esperarPistaParaAterrar();
+	}
+	else if(Plane::LIFTOFF==direction)
+	{
+		return gestor->esperarPistaParaDescolar();
+	}
+}
+
+INT * getAnimationEditText(INT idLane)
+{
+	if(idLane==1)
+	{
+		return Landing_Animate_Id;
+	}
+	else if(idLane==0)
+	{
+		return Takeoff_Animate_Id;
+	}
+
+}
+void doAnimation(Plane * plane, HWND hDlg, INT animationEditTexts)
+{
+		Edit_SetText(GetDlgItem(hDlg, animationEditTexts), plane->GetName());
+        Sleep(200);
+        Edit_SetText(GetDlgItem(hDlg, animationEditTexts), TEXT("   "));
+}
+void perform(HWND hDlg, HWND hList, HWND hEditList,Plane::PlaneDirection direction
+			 //,CountPlanesFunc cpf,LandOrLiftPlanesFunc lolpf,
+			 )
+{
+	Plane * plane = gestor->criarAviaoPara(direction);
+	ListBox_AddString(hList,plane->GetName());
+	//_itot_s(cpf(),buffer,sizeof(_TCHAR)*MAX_BUFFER,10);
+	loadIntoBufferPlanesCount(direction);
+	
+	Edit_SetText(hEditList,buffer);
+	
+	plane = executeLandOrListFunction(direction);
+	EnterCriticalSection(&csDelFromLB);
+	int temp = ListBox_FindStringExact(hList,0,plane->GetName());
+	ListBox_DeleteString(hList,temp);
+	LeaveCriticalSection(&csDelFromLB);
+
+	//_itot_s(cpf(),buffer,sizeof(_TCHAR)*MAX_BUFFER,10);
+	loadIntoBufferPlanesCount(direction);
+	Edit_SetText(hEditList,buffer);
+
+	INT * animationEditTexts = getAnimationEditText(plane->_idLane);
+	
+	if(plane->GetDirection()==Plane::LAND)
+	{
+		for (int i=0; i < 26; ++i) {
+			doAnimation(plane,hDlg,animationEditTexts[i]);
+		}
+	}
+	else if(plane->GetDirection()==Plane::LIFTOFF)
+	{
+		for(int i = 25; i>=0;--i)
+		{
+			doAnimation(plane,hDlg,animationEditTexts[i]);
+		}
+	}
+
+	gestor->libertarPista(plane);
+}
+
+
+
+DWORD WINAPI thAviaoAterrar(LPVOID param) 
+{
+    HWND hDlg = (HWND)param;
+	HWND hList = GetDlgItem(hDlg,IDC_LAND_LIST);
+	HWND hEditList = GetDlgItem(hDlg,IDC_N_LAND_LIST);
+	
+	perform(hDlg,hList,hEditList,Plane::LAND
+		//,&(gestor->getCountPlanesToLand),&(gestor->esperarPistaParaAterrar),
+		);
+
+    return 0;
+}
+
+DWORD WINAPI thAviaoDescolar(LPVOID p) 
+{
+    HWND hDlg = (HWND)p;
+	HWND hList = GetDlgItem(hDlg,IDC_LIFT_LIST);
+	HWND hEditList = GetDlgItem(hDlg,IDC_N_LIFT_LIST);
+
+	perform(hDlg,hList,hEditList,Plane::LIFTOFF);
+    return 0;
+}
+
+VOID WINAPI fecharOuAbrirPista(HWND hControl,INT idLane, HWND hStatic)
+{
+	Button_GetText(hControl,buffer,MAX_BUFFER);
+	if(buffer[0]=='A')
+	{
+		gestor->abrirPista(idLane);
+		Button_SetText(hControl,_T("Fechar"));
+		Static_SetText(hStatic,_T("Encontra-se aberta"));
+	}
+	else if(buffer[0]=='F')
+	{
+		gestor->fecharPista(idLane);
+		Button_SetText(hControl,_T("Abrir"));
+		Static_SetText(hStatic,_T("Encontra-se fechada"));
+	}
+}
+
+VOID WINAPI pistaParaAterrarOuDescolar(HWND hControl, INT idLane, HWND hStatic)
+{
+	Button_GetText(hControl,buffer,MAX_BUFFER);
+	if(buffer[0]=='A')
+	{
+		gestor->SetLanePriorityTo(Plane::LAND,idLane);
+		Button_SetText(hControl,_T("Descolar"));
+		Static_SetText(hStatic,_T("Dá prioridade a aterragens"));
+	}
+	else if(buffer[0]=='D')
+	{
+		gestor->SetLanePriorityTo(Plane::LIFTOFF,idLane);
+		Button_SetText(hControl,_T("Aterrar"));
+		Static_SetText(hStatic,_T("Dá prioridade a descolagens"));
+	}
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int n_planes;
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		gestor = new GestorDePistas(2);
+		gestor->SetLanePriorityTo(Plane::LAND,1);
+		gestor->SetLanePriorityTo(Plane::LIFTOFF,0);
+
+		Static_SetText(GetDlgItem(hDlg,IDC_PISTA1_LL),_T("Dá prioridade a aterragens"));
+		Static_SetText(GetDlgItem(hDlg,IDC_PISTA0_LL),_T("Dá prioridade a descolagens"));
+		Static_SetText(GetDlgItem(hDlg,IDC_PISTA1_OC),_T("Encontra-se aberta"));
+		Static_SetText(GetDlgItem(hDlg,IDC_PISTA0_OC),_T("Encontra-se aberta"));
+		// é necessário para trabalhar sobre a parte grafica,
+		//porque quando vai obter o indice do elemento a remover,
+		//pode haver um contextswitch que provoca erros.
+		InitializeCriticalSectionAndSpinCount(&csDelFromLB,5000);
+		_bFuracao = false;
+
+		Edit_SetText(GetDlgItem(hDlg, IDC_N_LAND), TEXT("0"));
+		Edit_SetText(GetDlgItem(hDlg, IDC_N_LIFT), TEXT("0"));
+		Edit_SetText(GetDlgItem(hDlg, IDC_N_LAND_LIST), TEXT("0"));
+		Edit_SetText(GetDlgItem(hDlg, IDC_N_LIFT_LIST), TEXT("0"));
+		
+		//Button_SetCheck(GetDlgItem(hDlg, IDC_CHECK1), TRUE);
+		//Button_SetCheck(GetDlgItem(hDlg, IDC_CHECK2), TRUE);
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+        switch ( LOWORD(wParam) ) {
+            case IDC_CREATE_LANDING:
+				Edit_GetText(GetDlgItem(hDlg,IDC_N_LAND),buffer,MAX_BUFFER);
+				n_planes = _ttoi(buffer);
+				for(int i = 0;i<n_planes;++i)
+				{
+					CreateThread(NULL, 0, thAviaoAterrar, (LPVOID)hDlg, 0, NULL);
+				}
+                break;
+            case IDC_CREATE_TAKEOFF:
+				Edit_GetText(GetDlgItem(hDlg,IDC_N_LIFT),buffer,MAX_BUFFER);
+				n_planes = _ttoi(buffer);
+				for(int i = 0;i<n_planes;++i)
+				{
+					CreateThread(NULL, 0, thAviaoDescolar, (LPVOID)hDlg, 0, NULL);
+				}
+                break;
+			case IDC_OPEN_LANE0:
+				fecharOuAbrirPista(GetDlgItem(hDlg,IDC_OPEN_LANE0),0,GetDlgItem(hDlg,IDC_PISTA0_OC));
+				break;
+			case IDC_OPEN_LANE1:
+				fecharOuAbrirPista(GetDlgItem(hDlg,IDC_OPEN_LANE1),1,GetDlgItem(hDlg,IDC_PISTA1_OC));
+				break;
+			case IDC_LIFT_LANE0:
+				pistaParaAterrarOuDescolar(GetDlgItem(hDlg,IDC_LIFT_LANE0),0,GetDlgItem(hDlg,IDC_PISTA0_LL));
+				break;
+			case IDC_LAND_LANE1:
+				pistaParaAterrarOuDescolar(GetDlgItem(hDlg,IDC_LAND_LANE1),1,GetDlgItem(hDlg,IDC_PISTA1_LL));
+				break;
+			case IDC_FURACAO:
+				if(!_bFuracao)
+				{
+					gestor->fecharPista(0);
+					Button_SetText(GetDlgItem(hDlg,IDC_OPEN_LANE0),_T("Abrir"));
+					Static_SetText(GetDlgItem(hDlg,IDC_PISTA0_OC),_T("Encontra-se fechada"));
+					gestor->fecharPista(1);
+					Button_SetText(GetDlgItem(hDlg,IDC_OPEN_LANE1),_T("Abrir"));
+					Static_SetText(GetDlgItem(hDlg,IDC_PISTA1_OC),_T("Encontra-se fechada"));
+					Static_SetText(GetDlgItem(hDlg,IDC_LBL_FURACAO),_T("AVISO: FURACÃO!"));
+				}else
+				{
+					Static_SetText(GetDlgItem(hDlg,IDC_LBL_FURACAO),_T(""));
+				}
+				_bFuracao = !_bFuracao;
+				gestor->alertaFuracao(_bFuracao);
+				break;
+            case IDCANCEL:
+                EndDialog(hDlg, LOWORD(wParam));
+			    return (INT_PTR)TRUE;
+        }
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
