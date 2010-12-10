a4d1ba84f17cddc90d6b1db51c195ad398200168
nuno.cancelo <nuno.cancelo@b139f23c-5e1e-54d6-eab5-85b03e268133>
2010-12-10 14:17:57 +0000

    Work in progress

--- trunk/SE1/SE1/nac/code/src/device/scb/SCB.h
+++ trunk/SE1/SE1/nac/code/src/device/scb/SCB.h
@@ -155,8 +155,25 @@ typedef struct _SCS{
 #define     __MAM_PARTIAL_ENABLE__                  ((const unsigned char ) 0x1 )
 #define     __MAM_DISABLE__                         ((const unsigned char ) 0x0 )
 
+#define     __MAMCR_MASK__                          ((const unsigned char ) 0x3 )
+
+#define     __MAMTIM_FETCH_1_CLOCK__                ((const unsigned char ) 0x1 )
+#define     __MAMTIM_FETCH_2_CLOCK__                ((const unsigned char ) 0x2 )
+#define     __MAMTIM_FETCH_3_CLOCK__                ((const unsigned char ) 0x3 )
 #define     __MAMTIM_FETCH_4_CLOCK__                ((const unsigned char ) 0x4 )
+#define     __MAMTIM_FETCH_5_CLOCK__                ((const unsigned char ) 0x5 )
+#define     __MAMTIM_FETCH_6_CLOCK__                ((const unsigned char ) 0x6 )
+#define     __MAMTIM_FETCH_7_CLOCK__                ((const unsigned char ) 0x7 )
+#define     __MAMTIM_MASK__                         ((const unsigned char ) 0x7 )
 
-
+/*
+  Table 35.
+  Suggestions for MAM timing selection 
+  system clock              Number of MAM fetch cycles in MAMTIM
+  < 20 MHz                    1 CCLK
+  20 MHz to 40 MHz            2 CCLK
+  40 MHz to 60 MHz            3 CCLK
+  >60 MHz                     4 CCLK
+*/
 
 #endif
