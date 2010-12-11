1b355fc39fb456fceebd7610569be22554835945
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@b139f23c-5e1e-54d6-eab5-85b03e268133>
2010-12-11 16:03:21 +0000

    refactoring

--- SE1/workplace/code/src/device/startup/startosc.h
+++ SE1/workplace/code/src/device/startup/startosc.h
@@ -1,18 +1,16 @@
 #ifndef STARTOSC_H
 #define STARTOSC_H
+#include "TYPES.h"
 #include "LPC2106.h"
 #include "SCB.h"
-
-
-#define    pPLL   ((pLPC_PLL) 0xE01FC080)     
-#define    pMAM   ((pLPC_MAM) 0xE01FC000)     
-   
+    
 
 #define     PLL_FEED()              {pPLL->FEED = __PLL_FEED_AA__; pPLL->FEED = __PLL_FEED_55__;}
 
 
-#define CCLK          (__PLL_M_VALUE__ * __FOSC__)
-#define FCCO          (__FOSC__ * __PLL_M_VALUE__ * __PLL_P_VALUE__ * 2)
+#define     CCLK          (__PLL_M_VALUE__ * __FOSC__)
+#define     FCCO          (__FOSC__ * __PLL_M_VALUE__ * __PLL_P_VALUE__ * 2)
+
 void StartOsc(void);
 
 #endif
