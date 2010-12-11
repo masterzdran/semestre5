79117caa5e63e159165a6fe7b869d3617a7f45ea
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@b139f23c-5e1e-54d6-eab5-85b03e268133>
2010-12-11 16:04:40 +0000

    ++

---
+++ SE1/workplace/code/src/device/GPIO/GPIO.c
@@ -0,0 +1,46 @@
+#include "GPIO.h"
+/*
+ * Table 58. Pin function select register bits (Page 66)
+ *   
+ * 00 Primary (default) function, typically GPIO port 
+ * 01 First alternate function 
+ * 10 Second alternate function 
+ * 11 Third alternate function 
+ * */
+
+/*The PINSEL0 register controls the functions of the pins 0-15*/
+static void gpio_PINSEL0(U32 mask){ 
+ __PINSEL0__ |= mask; 
+}
+
+/*The PINSEL1 register controls the functions of the pins 16-31*/
+static void gpio_PINSEL1(U32 mask){   
+  __PINSEL1__ |= mask;
+}
+
+void gpio_init(U32 pinsel0_mask,U32 pinsel1_mask){
+  gpio_PINSEL0(pinsel0_mask);
+  gpio_PINSEL1(pinsel1_mask);
+} 
+/*
+ *Table 62. (Page 72)
+ * GPIO port 0 Direction register (IO0DIR - address 0xE002 8008) bit description
+ * 0 Controlled pin is input.
+ * 1 Controlled pin is output.
+ * */
+void gpio_write(U32 mask, U32 value ){
+  pGPIO->IOSET = mask & value;  
+  pGPIO->IOCLR = ~(mask & value);
+}
+
+void gpio_set_direction(U32 mask, unsigned char direction){
+  pGPIO->IODIR &= (direction)?mask:~mask;
+}
+
+void gpio_clear(U32 mask){
+  pGPIO->IOCLR = mask; 
+}
+
+U32 gpio_read(U32 mask){
+    return  pGPIO->IOPIN& mask;
+}
