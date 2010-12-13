e9285d29fd6625ce6cab30eee5ce8b94e4c592a2
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@b139f23c-5e1e-54d6-eab5-85b03e268133>
2010-12-13 23:19:42 +0000

    refactor

--- SE1/workplace/code/src/tests/lcd-test/lcd_test.c
+++ SE1/workplace/code/src/tests/lcd-test/lcd_test.c
@@ -8,12 +8,23 @@
 
 int main(){
   gpio_init(0,0);
-//  gpio_set_direction(0x3FF00,GPIO_OUT);
- // gpio_set(0x3FF00);
-  timer_init(pTIMER0,CCLK/MICRO);
-  LCD_init(pTIMER0);
   
-  writeString("TESTE");
+ // gpio_set_direction(0x1F00,GPIO_OUT);
+  //gpio_clear(0x1F00,0x1F00);
+  //gpio_set(0x1F00);
+  timer_init(pTIMER0,58982400/MILI);
+  
+  
+  while (1){
+    gpio_set(0x2000);
+    timer_sleep_miliseconds(pTIMER0,500);
+    gpio_clear(0x2000,0x2000);
+    timer_sleep_miliseconds(pTIMER0,500);
+  }
+    
+  //LCD_init(pTIMER0);
+  
+  //writeString("TESTE");
   
   
   
