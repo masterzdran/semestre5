3dbb846b48c2783ad537e55148a44dbc15bf2349
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@b139f23c-5e1e-54d6-eab5-85b03e268133>
2010-12-30 15:19:44 +0000

    LoggerDLL.cpp

--- SO/Trabalhos/Trabalho 3/tmp_src/31401/LoggerDLL/Trabalho3/LoggerDLL.cpp
+++ SO/Trabalhos/Trabalho 3/tmp_src/31401/LoggerDLL/Trabalho3/LoggerDLL.cpp
@@ -2,15 +2,59 @@
 #include <windows.h>
 #include <tchar.h>
 
-static DWORD		_pageLimit	= 0;
-static LPVOID		_loggerBase = NULL;
+static DWORD		_pageLimit	= 	0;
+static LPVOID		_loggerBase = 	NULL;
 static SYSTEM_INFO	_systemInfo ;
-static DWORD		_pageSize	= 0;
+static DWORD		_pageSize	= 	0;
+stacic LPVOID		_loggerBaseFree = NULL;
+
+static DWORD		_nextToBeFreed	=	0;
+static DWORD		_nextToBeAlloc	=	0;
+
+
+#define __DEFAULT_SIZE__	1024
 
 void exitReport(LPTSTR message){
 	_tprintf(TEXT("Error!\nMessage: %s\nError Code: %ld\n"),message,GetLastError());
 	exit(0);
 }
+
+DWORD tlsIDX	= -1;
+
+static void _DLL_TlsInit()
+{
+    if ((tlsIDX = TlsAlloc()) == -1)  
+      FatalErrorSystem(_T("TlsAlloc failed!"));
+}
+
+static void _DLL_TlsEnd()
+{
+	if( tlsIDX != -1)
+		if( !TlsFree(indxRnd) ) 
+            FatalErrorSystem(_T("TlsFree failed!"));
+}
+
+static DWORD* _TlsInit()
+{
+	int *p = new int[1];  
+	*p = 1;
+	if (! TlsSetValue(tlsIDX, p)) 
+        FatalErrorSystem(_T("TlsSetValue failed!")); 
+	return p;
+}
+
+static void _TlsClose()
+{
+	int* p = (int*)TlsGetValue(tlsIDX); 
+	if ( p ) { 
+		delete p; 
+		p=NULL;
+		if ( !TlsSetValue(tlsIDX, p) ) 
+            FatalErrorSystem(_T("TlsSetValue failed!")); 
+	}
+}
+
+
 
 /**
  * Uma função para a iniciação do suporte do registo de histórico de informação, 
@@ -23,23 +67,37 @@ void CreateThreadLog(DWORD bytes){
 	GetSystemInfo(&_systemInfo);
 	_pageSize = _systemInfo.dwPageSize;
 	_pageLimit = bytes/_pageSize + 1;
-	_loggerBase = VirtualAlloc(NULL , _pageLimit*_pageSize , MEM_RESERVE , PAGE_NOACCESS); 
+	_loggerBase = VirtualAlloc(NULL , _pageLimit * _pageSize , MEM_RESERVE , PAGE_NOACCESS); 
 	if (_loggerBase == NULL)
 		exitReport((LPTSTR)"Error while use VirtualAlloc.");
+	
+	_loggerBaseFree = _loggerBase;
 }
 
 
 /**
  * Uma função para adicionar informação ao registo de histórico (AppendThreadLog(…)); 
  * */
-void AppendThreadLog();
+void AppendThreadLog(LPTSTR message){
+	if (_loggerBase == NULL)
+		CreateThreadLog(__DEFAULT_SIZE__);
+
+	DWORD size = sizeof(message);
+	//Usar controlo de excepções
+	//Usar memcopy para copiar a mensagem para o endereço de _loggerBaseFree
+	//Se falhar a excepção chamar a função que vai fazer commit da proxima pagina 
+	//ou paginas suficientes para agregar as páginas
+	//Actualizar o valor de _loggerBaseFree para o proximo byte apos ter sido colocada a mensagem
+
+
+}
 
 /**
  * Uma função para que permita libertar um bloco de informação mais antigo 
  * (segundo a lógica de um buffer circular), 
  * e.g.  FreeThreadLog(…, nBytes) o que significa que o espaço dos  nBytes mais antigos é libertado;
  * */
-void FreeThreadLog();
+void FreeThreadLog(DWORD bytes);
 
 
 /**
