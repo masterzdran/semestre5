d60687688b2f170b07ab335498b45cd1f8ab4f81
nuno.cancelo <nuno.cancelo@b139f23c-5e1e-54d6-eab5-85b03e268133>
2011-01-27 14:01:23 +0000

    .

---
+++ SE1/workplace/code/src/device/I2C/I2C.h
@@ -0,0 +1,72 @@
+#ifndef I2C_H
+#define I2C_h
+
+
+
+#define     pI2C      (((pLPC_I2C) 0xE001C000))
+
+typedef struct _I2C{
+  U8 CONTROL_SET_REGISTER;
+  U8  DUMMY01;
+  U16 DUMMY02;
+  
+  U8 STATUS_REGISTER;
+  U8  DUMMY03;
+  U16 DUMMY04;
+  
+  U8 DATA_REGISTER;
+  U8  DUMMY05;
+  U16 DUMMY06;
+  
+  U8 SLAVE_ADDRESS_REGISTER;
+  U8  DUMMY07;
+  U16 DUMMY08;
+  
+  U16 DUTY_CYCLE_HIGH_HALF_WORD_REGISTER;
+  U16 DUMMY09;  
+
+  U16 DUTY_CYCLE_LOW_HALF_WORD_REGISTER;
+  U16 DUMMY10;  
+
+  U8 CONTROL_CLEAR_REGISTER;
+  U8  DUMMY11;
+  U16 DUMMY12;
+}LPC_I2C,*pLPC_I2C;
+
+#define __I2CONSET_MASK__       0xFC    //bits 0,1 and 7 are reserved
+#define __I2EN_ENABLE__         0x80
+#define __I2C_STA_ENABLE__      0x10
+#define __I2C_STO_ENABLE__      0x20
+#define __I2C_SI_ENABLE__       0x08
+#define __I2C_AA_ENABLE__       0x04
+
+#define __I2CONCLR_MASK__       0x6C     //bits 0,1,4 and 7 are reserved
+#define __I2C_AAC_CLEAR__       0x04
+#define __I2C_SIC_CLEAR__       0x08
+#define __I2C_STAC_CLEAR__      0x10
+#define __I2C_I2ENC_CLEAR__     0x20
+
+#define __I2C_STATUS_MASK__     0xF8
+#define __I2DAT_MASK__          0xFF
+
+#define __I2ADR_MASK__          0xFF
+#define __I2ADR_CALL__          0x01
+#define __I2ADR_CALL_ADR_MASK__ 0xFE   
+
+#define __I2C_SCL_PIN__         ((U8)   1 << 0x2)
+#define __I2C_SDA_PIN__         ((U8)   1 << 0x3)
+
+#define __I2C_READ__            ((U8) 0x0)
+#define __I2C_WRITE__           ((U8) 0x1)
+
+void i2c_init();
+void i2c_start();
+void i2c_stop();
+void i2c_write_byte(U8 value);
+int i2c_slave_ack();
+U8 i2c_read_byte();
+void i2c_master_ack();
+void i2c_master_nack();
+
+
+#endif
