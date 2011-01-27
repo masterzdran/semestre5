e7b317cc9573f95f70bde6f842f162ce1cd987b1
nuno.cancelo <nuno.cancelo@b139f23c-5e1e-54d6-eab5-85b03e268133>
2011-01-27 15:23:14 +0000

    Remake...

--- SE1/workplace/code/src/peripherical/EEPROM/EEPROM.c
+++ SE1/workplace/code/src/peripherical/EEPROM/EEPROM.c
@@ -15,16 +15,14 @@
  * -----------------------------------------------------------------------------
  * 
  * */
+#include "EEPROM.h"
  
- 
-U8 eeprom_read_8(U32 address){
-
-}
+U8 eeprom_read_8(U32 address);
 U16 eeprom_read_16(U32 address);
 U32 eeprom_read_32(U32 address);
-void eeprom_read_block(U32 address, U8 * block, size_t size);
+void eeprom_read_block(U32 address, U8 * block, U8 size);
 void eeprom_write_8(U32 address, U8 value);
 void eeprom_write_16(U32 address, U16 value);
 void eeprom_write_32(U32 address, U32 value);
-size_t eeprom_write_block(U32 address, U8 * block, size_t size);
-U8 eeprom_checksum(U32 address, size_t size);
+U8 eeprom_write_block(U32 address, U8 * block, U8 size);
+U8 eeprom_checksum(U32 address, U8 size);
