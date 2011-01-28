3eeaf70b7a4784f4c23d68f033b857ef3a3425db
nuno.cancelo <nuno.cancelo@b139f23c-5e1e-54d6-eab5-85b03e268133>
2011-01-28 14:52:21 +0000

    WD

--- trunk/SE1/workplace/code/src/device/WD/WATCHDOG.c
+++ trunk/SE1/workplace/code/src/device/WD/WATCHDOG.c
@@ -9,6 +9,11 @@ void WATCHDOG_init(U32 value){
    * Start the watchdog by writing 0xAA followed by 0x55 to the WDFEED register.
    * Watchdog should be fed again before the watchdog counter underflows to prevent reset/interrupt.
    */
-pWatchDog->TIMER_CONSTANT = value;
+  pWatchDog->TIMER_CONSTANT = value;
 
 }
+Bool WD_isRunning(){return pWatchDog->MODE_REGISTER & __WDEN_MASK__;}
+void WD_Enable(){ pWatchDog->MODE_REGISTER |= __WDEN_MASK__;}
+void WD_Disable(){ pWatchDog->MODE_REGISTER &= ~(__WDEN_MASK__);}
+void WD_ResetEnable(){ pWatchDog->MODE_REGISTER |= _WDRESET_ENABLE__;}
+void WD_ResetDisable(){ pWatchDog->MODE_REGISTER &= ~(_WDRESET_ENABLE__)}
