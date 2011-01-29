86a2bb0aa911180516112779e07770b6ed036bd7
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@b139f23c-5e1e-54d6-eab5-85b03e268133>
2011-01-29 16:20:36 +0000

    TIMER Capture TIMER MATCH

--- SE1/workplace/code/src/device/TIMER/TIMER.c
+++ SE1/workplace/code/src/device/TIMER/TIMER.c
@@ -25,3 +25,32 @@ void timer_delay(pLPC_TIMER timer, U32 elapse){
     while(timer_elapsed(timer,time)<= elapse);
 }
 
+void TIMER_capture_init(pLPC_TIMER timer,TCapture timerCapture, U32 captureMask, U32 countNbr){
+  //Enable GPIO for Timer Capture or Else
+  if (timer  == pTIMER0 )
+    gpio_init(timerCapture,0);
+  else if (timer == pTIMER1)
+    gpio_init(0,timerCapture);
+  else {
+    return;
+  }
+  timer->TCR    = __TCR_DISABLE__|__TCR_RESET_ENABLE__;  //disable timer
+  timer->CCR   |= captureMask;                         //config counting method
+  timer->PR     = countNbr;                              //define count number  
+  timer->TCR    = __TCR_ENABLE__|__TCR_RESET_DISABLE__;  // enable timer  
+}
+
+void TIMER_match_init(pLPC_TIMER timer,TMatch timerMatch, U32 MatchMask, U32 countNbr){
+  //Enable GPIO for Timer Capture or Else
+  if (timer  == pTIMER0 )
+    gpio_init(timerMatch,0);
+  else if (timer == pTIMER1)
+    gpio_init(0,timerMatch);
+  else {
+    return;
+  }
+  timer->TCR    = __TCR_DISABLE__|__TCR_RESET_ENABLE__;  //disable timer
+  timer->MCR   |= MatchMask;                         //config counting method
+  timer->PR     = countNbr;                              //define count number  
+  timer->TCR    = __TCR_ENABLE__|__TCR_RESET_DISABLE__;  // enable timer  
+}
